177b1e0b80b2443b6a1df4e04ab6226dd197c6e5
05Konz <05konz994@gmail.com>
2023-09-08 21:21:16 -0500

    Fix react gui

--- cheats/reactGui.js
+++ cheats/reactGui.js
@@ -3535,7 +3535,7 @@
         }
         let iframe = document.querySelector("iframe");
         const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "(.+?)"/);
-        if (parseInt(time) <= 1693429947556 || iframe.contentWindow.confirm(error)) cheat();
+        if (parseInt(time) <= 1694225981451 || iframe.contentWindow.confirm(error)) cheat();
     }
     img.onerror = img.onabort = () => (img.src = null, cheat());
 })();
