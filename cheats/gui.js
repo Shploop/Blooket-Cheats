7adc24e8cc2353d35a5a3b318e261853078f5cc7
Benjamin Stewart <benjamin.stewart@duke.edu>
2023-10-05 16:44:50 -0500

    Decrease chance of Daily Rewards suspending (i hope)

--- cheats/gui.js
+++ cheats/gui.js
@@ -736,7 +736,7 @@
                 {
                     name: "Get Daily Rewards",
                     description: "Gets max daily tokens and xp",
-                    run: function () {
+                    run: async function () {
                         let i = document.createElement('iframe');
                         document.body.append(i);
                         window.alert = i.contentWindow.alert.bind(window);
@@ -744,27 +744,27 @@
                         if (!location.href.includes("play.blooket.com")) (alert("This cheat only works on play.blooket.com, opening a new tab."), window.open("https://play.blooket.com/"));
                         else {
                             const cache = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache),
-                                axios = cache.find((x) => x.exports?.a?.get).exports.a;
-                            axios.post("https://play.blooket.com/api/playersessions/solo", {
-                                gameMode: "Factory",
-                                questionSetId: ["60101da869e8c70013913b59", "625db660c6842334835cb4c6", "60268f8861bd520016eae038", "611e6c804abdf900668699e3", "60ba5ff6077eb600221b7145", "642467af9b704783215c1f1b", "605bd360e35779001bf57c5e", "6234cc7add097ff1c9cff3bd", "600b1491d42a140004d5215a", "5db75fa3f1fa190017b61c0c", "5fac96fe2ca0da00042b018f", "600b14d8d42a140004d52165", "5f88953cdb209e00046522c7", "600b153ad42a140004d52172", "5fe260e72a505b00040e2a11", "5fe3d085a529560004cd3076", "5f5fc017aee59500041a1456", "608b0a5863c4f2001eed43f4", "5fad491512c8620004918ace", "5fc91a9b4ea2e200046bd49a", "5c5d06a7deebc70017245da7", "5ff767051b68750004a6fd21", "5fdcacc85d465a0004b021b9", "5fb7eea20bd44300045ba495"][Math.floor(Math.random() * 24)]
-                            }).then(async ({ data: { t } }) => {
-                                await axios.post("https://play.blooket.com/api/playersessions/landings", { t });
-                                const { name, blook: { name: blookUsed } } = await cache.find(x => x.exports.a?.me).exports.a.me({}).catch(() => alert('There was an error getting user data.'));
-                                await axios.put("https://play.blooket.com/api/users/factorystats", {
-                                    blookUsed, t, name,
-                                    cash: Math.floor(Math.random() * 90000000) + 10000000,
-                                    correctAnswers: Math.floor(Math.random() * 500) + 500,
-                                    upgrades: Math.floor(Math.random() * 300) + 300,
-                                    mode: "Time-Solo",
-                                    nameUsed: "You",
-                                    place: 1,
-                                    playersDefeated: 0,
-                                }).then(console.log);
-                                axios.put("https://play.blooket.com/api/users/add-rewards", { t, name, addedTokens: 500, addedXp: 300 })
-                                    .then(({ data: { dailyReward } }) => alert(`Added max tokens and xp, and got ${dailyReward} daily wheel tokens!`))
-                                    .catch(() => alert('There was an error when adding rewards.'));
+                                axios = cache.find((x) => x.exports?.a?.get).exports.a,
+                                { data: { t } } = await axios.post("https://play.blooket.com/api/playersessions/solo", {
+                                    gameMode: "Factory",
+                                    questionSetId: ["60101da869e8c70013913b59", "625db660c6842334835cb4c6", "60268f8861bd520016eae038", "611e6c804abdf900668699e3", "60ba5ff6077eb600221b7145", "642467af9b704783215c1f1b", "605bd360e35779001bf57c5e", "6234cc7add097ff1c9cff3bd", "600b1491d42a140004d5215a", "5db75fa3f1fa190017b61c0c", "5fac96fe2ca0da00042b018f", "600b14d8d42a140004d52165", "5f88953cdb209e00046522c7", "600b153ad42a140004d52172", "5fe260e72a505b00040e2a11", "5fe3d085a529560004cd3076", "5f5fc017aee59500041a1456", "608b0a5863c4f2001eed43f4", "5fad491512c8620004918ace", "5fc91a9b4ea2e200046bd49a", "5c5d06a7deebc70017245da7", "5ff767051b68750004a6fd21", "5fdcacc85d465a0004b021b9", "5fb7eea20bd44300045ba495"][Math.floor(Math.random() * 24)]
+                                });
+                            await axios.post("https://play.blooket.com/api/playersessions/landings", { t });
+                            await axios.get("https://play.blooket.com/api/playersessions/questions", { params: { t } });
+                            const { name, blook: { name: blookUsed } } = await cache.find(x => x.exports.a?.me).exports.a.me({}).catch(() => alert('There was an error getting user data.'));
+                            await axios.put("https://play.blooket.com/api/users/factorystats", {
+                                blookUsed, t, name,
+                                cash: Math.floor(Math.random() * 90000000) + 10000000,
+                                correctAnswers: Math.floor(Math.random() * 500) + 500,
+                                upgrades: Math.floor(Math.random() * 300) + 300,
+                                mode: "Time-Solo",
+                                nameUsed: "You",
+                                place: 1,
+                                playersDefeated: 0,
                             });
+                            axios.put("https://play.blooket.com/api/users/add-rewards", { t, name, addedTokens: 500, addedXp: 300 })
+                                .then(({ data: { dailyReward } }) => alert(`Added max tokens and xp, and got ${dailyReward} daily wheel tokens!`))
+                                .catch(() => alert('There was an error when adding rewards.'));
                         }
                     }
                 },
@@ -3823,7 +3823,7 @@
         }
         let iframe = document.querySelector("iframe");
         const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "(.+?)"/);
-        if (parseInt(time) <= 1695682539562 || iframe.contentWindow.confirm(error)) cheat();
+        if (parseInt(time) <= 1696542129455 || iframe.contentWindow.confirm(error)) cheat();
     }
     img.onerror = img.onabort = () => (img.src = null, cheat());
 })();
